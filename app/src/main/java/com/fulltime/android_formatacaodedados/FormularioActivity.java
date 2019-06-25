d98b9b8ca43f5adc647e3b73f09b04245c55c95b
Josue Lopes <josue10599@gmail.com>
2019-06-25 13:24:20 -0300

    Melhoria na apresentação do erro ao validar campo

--- app/src/main/java/com/fulltime/android_formatacaodedados/FormularioActivity.java
+++ app/src/main/java/com/fulltime/android_formatacaodedados/FormularioActivity.java
@@ -1,24 +1,32 @@
 package com.fulltime.android_formatacaodedados;
 
+import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class FormularioActivity extends AppCompatActivity {
 
-    TextInputLayout campoNomeCompleto;
-    TextInputLayout campoCpf;
-    TextInputLayout campoTelefone;
-    TextInputLayout campoEmail;
-    TextInputLayout campoSenha;
-    Button botaoCadastrar;
+    private TextInputLayout campoNomeCompleto;
+    private TextInputLayout campoCpf;
+    private TextInputLayout campoTelefone;
+    private TextInputLayout campoEmail;
+    private TextInputLayout campoSenha;
+    private Button botaoCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
         populaCampoDaActivity();
+
+        validaCampoDeTexto(campoNomeCompleto);
+        validaCampoDeTexto(campoCpf);
+        validaCampoDeTexto(campoTelefone);
+        validaCampoDeTexto(campoEmail);
+        validaCampoDeTexto(campoSenha);
     }
 
     private void populaCampoDaActivity() {
@@ -28,5 +36,21 @@ public class FormularioActivity extends AppCompatActivity {
         campoEmail = findViewById(R.id.formulario_cadastro_email);
         campoSenha = findViewById(R.id.formulario_cadastro_senha);
         botaoCadastrar = findViewById(R.id.formulario_cadastro_botao_cadastrar);
+    }
+
+    private void validaCampoDeTexto(final TextInputLayout campoDeTexto) {
+        final EditText editTextDoCampoText = campoDeTexto.getEditText();
+        if (editTextDoCampoText != null) {
+            editTextDoCampoText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    String textoDoCampo = editTextDoCampoText.getText().toString();
+                    if (!hasFocus) {
+                        if (textoDoCampo.isEmpty()) campoDeTexto.setError(getString(R.string.campo_obrigratorio));
+                        else campoDeTexto.setErrorEnabled(false);
+                    }
+                }
+            });
+        }
     }
 }
