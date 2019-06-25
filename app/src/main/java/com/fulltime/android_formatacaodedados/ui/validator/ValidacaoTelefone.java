afb5eecd414ede977a2094bf8560e3ec57f8f846
Josue Lopes <josue10599@gmail.com>
2019-06-25 18:13:22 -0300

    Implementado a classe ValidacaoTelefone

---
+++ app/src/main/java/com/fulltime/android_formatacaodedados/ui/validator/ValidacaoTelefone.java
@@ -0,0 +1,30 @@
+package com.fulltime.android_formatacaodedados.ui.validator;
+
+import android.support.design.widget.TextInputLayout;
+
+import com.fulltime.android_formatacaodedados.R;
+
+public class ValidacaoTelefone extends ValidacaoPadrao{
+
+    private final TextInputLayout campoTelefone;
+    private final String telefoneDigitado;
+
+    public ValidacaoTelefone(TextInputLayout campoTelefone) {
+        super(campoTelefone);
+        this.campoTelefone = campoTelefone;
+        this.telefoneDigitado = getTextoDigitadoNoCampo(campoTelefone);
+    }
+
+    public boolean valida() {
+        if (campoVazio()) return false;
+        if (quantidadeDigitosInvalida()) return false;
+        removeErro();
+        return true;
+    }
+
+    private boolean quantidadeDigitosInvalida() {
+        if (telefoneDigitado.length() == 10 || telefoneDigitado.length() == 11) return false;
+        campoTelefone.setError(campoTelefone.getContext().getText(R.string.erro_quantidade_digitos_telefone));
+        return true;
+    }
+}
